04ad5b6768f36e8f904a757cf08ac1df76f08183
PerfidusPistor <32550526+PerfidusPistor@users.noreply.github.com>
2017-12-08 12:35:19 +0100

    Add files via upload
    added switch commands

--- main.c
+++ main.c
@@ -2,16 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NUM_COMMANDS 2+1 // 1 is the noCmd for coherence
+
+
     char time1[256], time2[256];
     int timeGet1, timeGet2;
 
-int takeField(FILE *fp, char *field);
+    // command from input and conversion
+typedef enum{
+noCmd,hr,cad
+}command;
+
+char *cmdName[NUM_COMMANDS]={"noCmd","hr","cad"};
+
+command convertCommand(char *inputCmd);
+
+//functions declarations
+
+int takeField(FILE *fp, command field);
+
 
 int main(int argc, char *argv[])
 {
     FILE *fin1, *fin2, *fout;
     char f1Line[2048]="";
     char f2Line[2048]="";
+    char inputCmd[256];
+    command cmd;
 
     printf("\t--------------------------------------\n");
     printf("\t----------GPX Merger by Ivan----------\n");
@@ -27,6 +44,12 @@ int main(int argc, char *argv[])
     fin2 = fopen(argv[2],"r");
     fout = fopen("merged.gpx","w");
 
+    //field to merge
+    printf("what field do you want to merge: ");
+    scanf("%s", inputCmd);
+    cmd = convertCommand(inputCmd);
+
+
     for(;fscanf(fin1,"%s",f1Line)!=EOF;){
 
         if(strncmp(f1Line,"<time>",6)==0 && timeGet1!=0){ //taking time value (just a check)
@@ -38,7 +61,7 @@ int main(int argc, char *argv[])
 
         if(strncmp(f1Line,"<gpxtpx:atemp>",14)==0){
 
-        sprintf(f2Line," <gpxtpx:hr>%d</gpxtpx:hr>", takeField(fin2,NULL));
+        sprintf(f2Line," <gpxtpx:hr>%d</gpxtpx:hr>", takeField(fin2,cmd));
 
         //copy to new file (fout)
         fprintf(fout,"%s\n%s\n", f1Line, f2Line);
@@ -62,10 +85,11 @@ int main(int argc, char *argv[])
 }
 
 
-int takeField(FILE *fp, char *field){
+int takeField(FILE *fp, command field){
 
     int value;
     char buffer[2048];
+    int flag=0;
 
         for(;fscanf(fp,"%s",buffer)!=EOF;){
             if(strncmp(buffer,"<time>",6)==0 && timeGet2!=0){ //taking time value (just a check)
@@ -79,16 +103,37 @@ int takeField(FILE *fp, char *field){
             }
 
             }
+            switch (field){
 
-            if(strncmp(buffer,"<gpxtpx:hr>",11)==0){ // field to merge (health rate)
+                case 1:
 
-            sscanf(buffer,"<gpxtpx:hr>%d</gpxtpx:hr>", &value);
+                   if(strncmp(buffer,"<gpxtpx:hr>",11)==0){ // field to merge
+                    sscanf(buffer,"<gpxtpx:hr>%d</gpxtpx:hr>", &value);
+                    flag=1;
+                   }
+
+                break;
+
+                default:
+                break;
+
+            }
+
+        if(flag==1) // the field has been found, exit
             break;
 
             }
 
-
-        }
-
     return value;
+}
+
+command convertCommand(char *inputCmd){
+    command cmd=0; //setup to no input command
+    int i;
+
+    for(i=0; i<NUM_COMMANDS; i++)
+        if(strcmp(inputCmd,cmdName[i])==0)
+            cmd=i;
+
+    return cmd;
 }
